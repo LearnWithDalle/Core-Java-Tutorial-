ec51b7d6b8f1b40bf75a2a1b97b687a435f716a5
dimond077 <dallemahesh09@gmail.com>
2024-01-21 21:58:47 +0530

    Update Dairy.java

--- Chapter 3 - Inheritance, Package and Collection/VIDEO 30/Dairy.java
+++ Chapter 3 - Inheritance, Package and Collection/VIDEO 30/Dairy.java
@@ -1,45 +1,60 @@
 // multiLevel Inheritance : methode overRiding, Handle multiLevel Constracter
 
 class Dairymilk {
-    int i;
-    Dairymilk(int i){
+    final int i;
+
+    Dairymilk(int i) {
         this.i = i;
         System.out.println(" Enjoy Dairy milk..........!!");
     }
-    void Display(){
+
+    void Display() {
         System.out.println("\n Kuch Mithaa ho jay. DairyMilk ");
     }
-    
+
 }
 
-class Dairymilk_Silk extends Dairymilk{
+class Dairymilk_Silk extends Dairymilk {
     int j;
-    Dairymilk_Silk(int i, int j){
+
+    Dairymilk_Silk(int i, int j) {
         super(i);
         this.j = j;
-        System.out.println(" Enjoy Dairy milk Silk..........!!");
+        System.out.println(" Enjoy Dairy milk Silk..........!!" + i + j);
     }
-    void Display(){
+
+    void Display() {
         System.out.println("\n Kuch Mithaa ho jay. DairyMilk Silk..");
     }
 }
 
-class Dairymilk_Hazalnut extends Dairymilk_Silk{
+class Dairymilk_Hazalnut extends Dairymilk_Silk {
     int k;
-    Dairymilk_Hazalnut(int i, int j, int k){
+
+    Dairymilk_Hazalnut(int i, int j, int k) {
         super(i, j);
         this.k = k;
-        System.out.println("  Dairy milk Hazulnut..........Enjoy !!");
+        System.out.println("  Dairy milk Hazulnut..........Enjoy !!" + i + j);
     }
-    void Display(){
+
+    void Display() {
         System.out.println("\n Kuch Mithaa ho jay. DairyMilk Hazulnut..");
     }
 }
 
 public class Dairy {
     public static void main(String[] args) {
+        System.out.println("\n_______________________________");
+        Dairymilk d1 = new Dairymilk(100);
+        d1.Display();
+        System.out.println("\n_______________________________");
+        Dairymilk_Silk d2 = new Dairymilk_Silk(200, 300);
+        d2.Display();
+        System.out.println("\n_______________________________");
+        Dairymilk_Hazalnut d3 = new Dairymilk_Hazalnut(300, 400, 500);
+        d3.Display();
 
     }
 
-    
 }
+
